487849c1544cf7b67b5e5f60ffb2fefee602ebfe
Jake Owens <Jowens@lawn-143-215-80-235.lawn.gatech.edu>
2017-03-16 17:34:49 -0400

    thursday 1

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,6 +1,3 @@
-
-
-
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyD4epD9MrhBlD5dqh3wgH5OTRj1cTdQDKM",
@@ -17,9 +14,16 @@ var dataRef = firebase.database();
 //set initial values for table
 var trainName = "";
 var trainDest = "";
-var firstTrainX = "";
-var trainFreq = "";
-$('input.form-control').val("");
+var firstTrainX = 0;
+var trainFreq = 0;
+var currentTime = moment();
+var trainArrival=0;
+var  minsAway= 0;
+//var firstTrainXconvert = moment(firstTrainX, "hh:mm").subtract(1,"years");
+    //console.log("1stTrainX "+ firstTrainXconvert);
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+
 
 //capture button click
 $("#submit-data").on("click", function(eventObject) {
@@ -27,10 +31,10 @@ $("#submit-data").on("click", function(eventObject) {
 
     trainName = $("#trainName").val().trim();
     trainDest = $("#destination").val().trim();
-    firstTrainX = $("#firstTrainTime").val().trim();
+    firstTrainX = $("#firstTrainX").val();
     trainFreq = $("#frequency").val().trim();
-
-    dataRef.ref().push({
+   
+       dataRef.ref().push({
     	trainName: trainName,
     	trainDest: trainDest,
     	firstTrainX: firstTrainX,
@@ -39,7 +43,9 @@ $("#submit-data").on("click", function(eventObject) {
     
 //$('input.className:form-control').val("");
     });
-        $('#myForm').trigger("reset");
+
+   
+           $('#myForm').trigger("reset");
 //$('#trainName').empty();
 //console.log('#trainName');
     return false;
@@ -54,11 +60,33 @@ dataRef.ref().on("child_added", function(childSnapshot) {
 	var trainDest = childSnapshot.val().trainDest;
 	var firstTrainX = childSnapshot.val().firstTrainX;
 	var trainFreq = childSnapshot.val().trainFreq;
-	var  minsAway= 0;
-	var  trainArr= 0;
+	//var  minsAway= 0;
+	//var  trainArrival= 0;
 
-	$("#trainInfo").append("<tr><td>" + trainName + "</td><td>" + trainDest+"</td><td>" + trainFreq+"</td><td>" +firstTrainX+  "</td><td>" + minsAway+"</td></tr>");
-$('input.classname:form-control').val("");
+
+    var firstTrainXconvert = moment(firstTrainX, "hh:mm").subtract(1,"years");
+    console.log("train convert " +firstTrainXconvert);
+    var firstToNowTX= moment().diff(moment(firstTrainXconvert), "minutes");
+    console.log(firstToNowTX);
+
+var tRemainder = firstToNowTX % trainFreq;
+    console.log(tRemainder);
+
+     var minsAway = trainFreq - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + minsAway);
+
+    // Next Train
+    var trainArrival = moment().add(minsAway, "minutes");
+    console.log("ARRIVAL TIME: " + moment(trainArrival).format("hh:mm"));
+ 
+
+
+
+
+
+
+	$("#trainInfo").append("<tr><td>" + trainName + "</td><td>" + trainDest+"</td><td>" + trainFreq+"</td><td>" + moment(trainArrival).format("hh:mm")+  "</td><td>" + minsAway+"</td></tr>");
+//$('input.classname:form-control').val("");
 });
 
 
